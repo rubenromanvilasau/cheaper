0f87c28654a7a71896e8dad2e6e474d0dcc3fece
RubenRoman <ruben.roman@forcast.cl>
2023-04-13 23:52:01 -0400

    :rocket: Public folder updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,13 +5,13 @@ export const Footer = () => {
         <footer>
 
             <div className="">
-                <img className="logo" src="assets/cheaper.svg" alt="" />
+                <img className="logo" src="/assets/cheaper.svg" alt="" />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit amet illo error voluptatum, suscipit dolor?</p>
                 <div className="social">
-                    <img src="assets/facebook-logo.svg" alt="" />
-                    <img src="assets/twitter-logo.svg" alt="" />
-                    <img src="assets/google-logo.svg" alt="" />
-                    <img src="assets/twitch-logo.svg" alt="" />
+                    <img src="/assets/facebook-logo.svg" alt="" />
+                    <img src="/assets/twitter-logo.svg" alt="" />
+                    <img src="/assets/google-logo.svg" alt="" />
+                    <img src="/assets/twitch-logo.svg" alt="" />
                 </div>
             </div>
 
@@ -43,7 +43,7 @@ export const Footer = () => {
                 <p className="title">Subscribe us</p>
                 <div className="input-subscribe">
                     <input type="text" />
-                    <button><img src="assets/send.svg" alt="" /></button>
+                    <button><img src="/assets/send.svg" alt="" /></button>
                 </div>
             </div>
 
